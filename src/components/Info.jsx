5e5acd5430ba31146e7fac7c6080b7bc0e561546
Farid Benmebarek <farid.benmebarek@hotmail.fr>
2019-05-25 11:27:00 +0200

    ajout du component Info

---
+++ src/components/Info.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Info extends Component {
+  render () {
+    return (
+        <div className="btn-group ">
+          <h4 className="subTitle">Nouveau Devis</h4>
+          <button className="btn btn-primary">Créer</button>
+        </div>
+    )
+  }
+}
+
+export default Info
