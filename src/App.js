f5d269c805aec15b3c39b0a5c160f964cf6a9966
Farid Benmebarek <farid.benmebarek@hotmail.fr>
2019-05-25 11:26:17 +0200

    transformation App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import React from 'react'
+import React, {Component} from 'react'
 import './App.css'
 
-function App () {
-  return (
-    <div className="container">
-      <div className="header">
-        <h1>Générateur de Devis</h1>
+import Info from './components/Info'
+
+class App extends Component{
+  state = {
+    showForm: true
+  }
+
+  render () {
+    return (
+      <div className="container">
+        <div className="header">
+          <h1>Générateur de Devis</h1>
+        </div>
+
+        <Info/>
       </div>
-      <div className="btn-group ">
-        <h4 className="subTitle">Nouveau Devis</h4>
-        <button className="btn btn-primary">Créer</button>
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default App
