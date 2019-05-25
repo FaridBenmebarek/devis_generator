e1c0cd77470b6f0a08dc1bc886c7f4f1716f5866
Farid Benmebarek <farid.benmebarek@hotmail.fr>
2019-05-25 11:04:40 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="header">
+        <h1>Générateur de Devis</h1>
+      </div>
+      <div className="btn-group ">
+        <h4 className="subTitle">Nouveau Devis</h4>
+        <button className="btn btn-primary">Créer</button>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
